1e19c494dcdd79a54e4b012892bc5650e050dc68
Dmytro Konstantinov <umka.dk@gmail.com>
2017-01-17 12:55:43 +0000

    Implemented support for Highlight.js

--- JSONav.safariextension/JSONav.js
+++ JSONav.safariextension/JSONav.js
@@ -5,8 +5,22 @@
 
 (function (object, $, undefined) {
 
+    var config = {
+        rawId:    'raw',
+        navId:    'nav',
+        tabstops: 4,
+        theme:    'default',
+    };
+
     object.init = function (jsonObject) {
         console.log(jsonObject);
+
+        var rawDOM = document.getElementById(config.rawId),
+            navDOM = document.getElementById(config.navId);
+
+        rawDOM.innerHTML = JSON.stringify(jsonObject, null, config.tabstops);
+
+        hljs.highlightBlock(rawDOM);
     };
 
 }( this.JSONav = this.JSONav || {} ));
